84541cf70c5e753d99230aeca97fbc4077a59667
tyleroakleaf <4921516+toakleaf@users.noreply.github.com>
2019-08-22 14:05:06 -0400

    got it working

--- dist/js/canvas.bundle.js
+++ dist/js/canvas.bundle.js
@@ -110,10 +110,11 @@ var ctx = canvas.getContext('2d');
 var mouse = {
   x: innerWidth / 2,
   y: innerHeight / 2
-};
-var colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66'];
-canvas.width = window.innerWidth;
+  // const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
+};canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+ctx.fillStyle = 'hsl(280, 100%, 10%)';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 // Audio
 var actx = new AudioContext();
@@ -147,37 +148,54 @@ canvas.addEventListener('click', function (event) {
 });
 
 // Implementation
-var shapes = void 0;
-function init() {
-  shapes = [];
+// let shapes
+// function init() {
+//   shapes = []
 
-  for (var i = 0; i < 400; i++) {
-    var radius = _utils2.default.randomIntFromRange(10, 50);
-    var x = _utils2.default.randomIntFromRange(0 + radius, canvas.width - radius);
-    var y = _utils2.default.randomIntFromRange(0 + radius, canvas.height - radius);
-    shapes.push(new _objects.Shape(ctx, x, y, radius, _utils2.default.randomColor(colors)));
-  }
-}
+//   for (let i = 0; i < 400; i++) {
+//     const radius = utils.randomIntFromRange(10, 50)
+//     const x = utils.randomIntFromRange(0 + radius, canvas.width - radius)
+//     const y = utils.randomIntFromRange(0 + radius, canvas.height - radius)
+//     shapes.push(new Shape(ctx, x, y, radius, utils.randomColor(colors)))
+//   }
+// }
 
 // Animation Loop
 var refTime = Date.now();
 var elapsedTime = 0;
+var imageData = void 0;
 function animate() {
   // requestAnimationFrame callback aims for a 60 FPS callback rate but doesn’t guarantee it, so manual track elapsed time
   requestAnimationFrame(animate);
+
+  imageData = ctx.getImageData(0, 0, canvas.width, canvas.height - 1);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.putImageData(imageData, 0, 1);
+
   elapsedTime = Date.now() - refTime;
 
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
   // slow down fft to only run every 200 ms
-  if (elapsedTime >= 200) {
+  if (elapsedTime >= 100) {
     refTime = Date.now();
 
     analyser.getByteFrequencyData(data);
-    // console.log(data)
-    data.forEach(function (bin) {
-      // console.log(bin)
+    data.forEach(function (amplitude, i) {
+      var rat = amplitude / 255;
+      var hue = Math.round((rat * 120 + 280) % 360);
+      var sat = '100%';
+      var lit = 10 + 70 * rat;
+      ctx.beginPath();
+      ctx.strokeStyle = 'hsl(' + hue + ', ' + sat + ', ' + lit + '%)';
+      ctx.moveTo(i * canvas.width / data.length, 0);
+      // ctx.moveTo(x, H - i * h)
+      ctx.lineTo(canvas.width / data.length + i * canvas.width / data.length, 0);
+      // ctx.lineTo(x, H - (i * h + h))
+      ctx.stroke();
     });
+  } else {
+    var topRow = imageData = ctx.getImageData(0, 1, canvas.width, 2);
+    ctx.putImageData(imageData, 0, 0);
   }
 
   // console.log(data)
@@ -185,13 +203,13 @@ function animate() {
   //   shape.update()
   // })
 
-  if (actx.state === 'suspended') {
-    ctx.fillStyle = '#fff';
-    ctx.fillText('CLICK TO BEGIN', mouse.x, mouse.y);
-  }
+  // if (actx.state === 'suspended') {
+  //   ctx.fillStyle = '#fff'
+  //   ctx.fillText('CLICK TO BEGIN', mouse.x, mouse.y)
+  // }
 }
 
-init();
+// init()
 animate();
 
 /***/ }),
--- src/js/canvas.js
+++ src/js/canvas.js
@@ -8,9 +8,11 @@ const mouse = {
   x: innerWidth / 2,
   y: innerHeight / 2
 }
-const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
+// const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
+ctx.fillStyle = 'hsl(280, 100%, 10%)'
+ctx.fillRect(0, 0, canvas.width, canvas.height)
 
 // Audio
 const actx = new AudioContext()
@@ -44,37 +46,57 @@ canvas.addEventListener('click', event => {
 })
 
 // Implementation
-let shapes
-function init() {
-  shapes = []
+// let shapes
+// function init() {
+//   shapes = []
 
-  for (let i = 0; i < 400; i++) {
-    const radius = utils.randomIntFromRange(10, 50)
-    const x = utils.randomIntFromRange(0 + radius, canvas.width - radius)
-    const y = utils.randomIntFromRange(0 + radius, canvas.height - radius)
-    shapes.push(new Shape(ctx, x, y, radius, utils.randomColor(colors)))
-  }
-}
+//   for (let i = 0; i < 400; i++) {
+//     const radius = utils.randomIntFromRange(10, 50)
+//     const x = utils.randomIntFromRange(0 + radius, canvas.width - radius)
+//     const y = utils.randomIntFromRange(0 + radius, canvas.height - radius)
+//     shapes.push(new Shape(ctx, x, y, radius, utils.randomColor(colors)))
+//   }
+// }
 
 // Animation Loop
 let refTime = Date.now()
 let elapsedTime = 0
+let imageData
 function animate() {
   // requestAnimationFrame callback aims for a 60 FPS callback rate but doesn’t guarantee it, so manual track elapsed time
   requestAnimationFrame(animate)
+
+  imageData = ctx.getImageData(0, 0, canvas.width, canvas.height - 1)
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+  ctx.putImageData(imageData, 0, 1)
+
   elapsedTime = Date.now() - refTime
 
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
-
   // slow down fft to only run every 200 ms
-  if (elapsedTime >= 200) {
+  if (elapsedTime >= 100) {
     refTime = Date.now()
 
     analyser.getByteFrequencyData(data)
-    // console.log(data)
-    data.forEach(bin => {
-      // console.log(bin)
+    data.forEach((amplitude, i) => {
+      let rat = amplitude / 255
+      let hue = Math.round((rat * 120 + 280) % 360)
+      let sat = '100%'
+      let lit = 10 + 70 * rat
+      ctx.beginPath()
+      ctx.strokeStyle = `hsl(${hue}, ${sat}, ${lit}%)`
+      ctx.moveTo((i * canvas.width) / data.length, 0)
+      // ctx.moveTo(x, H - i * h)
+      ctx.lineTo(
+        canvas.width / data.length + (i * canvas.width) / data.length,
+        0
+      )
+      // ctx.lineTo(x, H - (i * h + h))
+      ctx.stroke()
     })
+  } else {
+    let topRow = (imageData = ctx.getImageData(0, 1, canvas.width, 2))
+    ctx.putImageData(imageData, 0, 0)
   }
 
   // console.log(data)
@@ -82,11 +104,11 @@ function animate() {
   //   shape.update()
   // })
 
-  if (actx.state === 'suspended') {
-    ctx.fillStyle = '#fff'
-    ctx.fillText('CLICK TO BEGIN', mouse.x, mouse.y)
-  }
+  // if (actx.state === 'suspended') {
+  //   ctx.fillStyle = '#fff'
+  //   ctx.fillText('CLICK TO BEGIN', mouse.x, mouse.y)
+  // }
 }
 
-init()
+// init()
 animate()
